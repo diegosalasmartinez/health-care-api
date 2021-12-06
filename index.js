229542032f66ac435ec654f449e8dff89b925553
Diego Salas <diesalasmart@gmail.com>
2021-12-06 02:09:53 -0500

    initialize server

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+import userRoutes from './src/routes/UserRoutes.js'
+import doctorRoutes from './src/routes/DoctorRoutes.js'
+
+const app = express();
+
+app.use(express.json());
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+
+app.use('/users', userRoutes);
+app.use('/doctors', doctorRoutes);
+
+
+const CONNECTION_URL = "mongodb+srv://admin:adminpasswordDGYNL@cluster0.dcvkp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => {
+        console.log(`Server running on port: ${PORT}`);
+    }))
+    .catch((e) => console.log(e.message));
