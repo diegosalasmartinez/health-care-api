ec4d591302f941dd4f383cdfe5015913d2d4f079
Diego Salas <diesalasmart@gmail.com>
2021-12-06 17:06:54 -0500

    delete create doctor

--- src/routes/DoctorRoutes.js
+++ src/routes/DoctorRoutes.js
@@ -1,9 +1,8 @@
 import express from 'express'
-import { getDoctors, createDoctor } from '../controllers/DoctorController.js'
+import { getDoctors } from '../controllers/DoctorController.js'
 
 const router = express.Router();
 
 router.get('/', getDoctors);
-router.post('/add', createDoctor);
 
 export default router;
