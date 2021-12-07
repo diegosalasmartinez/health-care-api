b8572371b3a4d4f8134233e39b237bccbf6fe95f
Diego Salas <diesalasmart@gmail.com>
2021-12-07 12:02:39 -0500

    add update and delete routes

--- src/routes/PatientRoutes.js
+++ src/routes/PatientRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { getPatients, createPatient } from '../controllers/PatientController.js'
+import { getPatients, createPatient, updatePatient, deletePatient } from '../controllers/PatientController.js'
 
 const router = express.Router();
 
 router.get('/', getPatients);
 router.post('/add', createPatient);
+router.post('/:id', updatePatient);
+router.post('/delete/:id', deletePatient);
 
 export default router;
