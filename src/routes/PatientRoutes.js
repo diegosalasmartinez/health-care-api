40f981174074c6fabaaa5d36534548c63a6a4579
Diego Salas <diesalasmart@gmail.com>
2021-12-07 01:35:44 -0500

    import post route

--- src/routes/PatientRoutes.js
+++ src/routes/PatientRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { getPatients } from '../controllers/PatientController.js'
+import { getPatients, createPatient } from '../controllers/PatientController.js'
 
 const router = express.Router();
 
 router.get('/', getPatients);
+router.post('/add', createPatient);
 
 export default router;
