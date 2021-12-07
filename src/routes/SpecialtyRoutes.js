1718590cc4991de31fd095e8a860ba8dffff1280
Diego Salas <diesalasmart@gmail.com>
2021-12-07 16:49:45 -0500

    controller and routes files created

---
+++ src/routes/SpecialtyRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import { getSpecialties, createSpecialty, updateSpecialty, deleteSpecialty } from '../controllers/SpecialtyController.js'
+
+const router = express.Router();
+
+router.get('/', getSpecialties);
+router.post('/add', createSpecialty);
+router.post('/:id', updateSpecialty);
+router.post('/delete/:id', deleteSpecialty);
+
+export default router;
