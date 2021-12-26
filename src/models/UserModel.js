ae2d9d4525679abbd9094abc66ea4e481889b4ad
Diego Salas <diesalasmart@gmail.com>
2021-12-26 18:17:11 -0500

    update usermodel to create token and encrypt password

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
     personId: {
@@ -11,7 +13,8 @@ const userSchema = mongoose.Schema({
     },
     username: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     password: {
         type: String,
@@ -26,5 +29,22 @@ const userSchema = mongoose.Schema({
         default: true
     }
 })
+
+userSchema.pre('save', async function () {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+})
+
+userSchema.methods.createJWT = function () {
+    return jwt.sign(
+        { userId: this._id },
+        process.env.JWT_SECRET,
+        { expiresIn: 300 }
+    )    
+}
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
 
 module.exports = mongoose.model('User', userSchema)
