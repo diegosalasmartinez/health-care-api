b3c8f3721511e5b861e27f7efe16eb95efcd5012
Diego Salas <diesalasmart@gmail.com>
2021-12-26 18:15:58 -0500

    update controller to use async errors dependencie

--- src/controllers/PatientController.js
+++ src/controllers/PatientController.js
@@ -3,28 +3,24 @@ const Person = require('../models/PersonModel')
 const Patient = require('../models/PatientModel')
 
 const getPatients = async (req, res) => {
-    try {
-        const patients = await Patient.aggregate(
-            [
-                {
-                    $match: { active: { $eq: true } }
-                },
-                {
-                    $lookup: {
-                        from: "people",
-                        localField: "personId",
-                        foreignField: "_id",
-                        as: "personInfo",
-                    }
-                },
-                {
-                    $unwind: "$personInfo"
+    const patients = await Patient.aggregate(
+        [
+            {
+                $match: { active: { $eq: true } }
+            },
+            {
+                $lookup: {
+                    from: "people",
+                    localField: "personId",
+                    foreignField: "_id",
+                    as: "personInfo",
                 }
-            ])
-        res.status(200).json(patients);
-    } catch(e) {
-        res.status(404).json({message: e.message});
-    }
+            },
+            {
+                $unwind: "$personInfo"
+            }
+        ])
+    res.status(200).json(patients);
 }
 
 const createPatient = async (req, res) => {
@@ -38,37 +34,31 @@ const createPatient = async (req, res) => {
         phone: personInfo.phone, 
         sex: personInfo.sex
     })
+    const personCreated = await newPerson.save();
 
-    try {
-        const personCreated = await newPerson.save();
-        const newPatient = new Patient({
-            personId: personCreated._id,
-            clinicHistory: {
-                reason: clinicHistory.reason,
-                currentIllness: clinicHistory.currentIllness,
-                historyDesease: clinicHistory.historyDesease,
-                alcohol: clinicHistory.alcohol,
-                smoke: clinicHistory.smoke,
-                drugs: clinicHistory.drugs,
-                sexuality: clinicHistory.sexuality,
-                others: clinicHistory.others
-            },
-            code: patient.code,
-            allergies: patient.allergies,
-            address: patient.address,
-            birthday: patient.birthday,
-            occupation: patient.occupation,
-            civilStatus: patient.civilStatus,
-            nationality: patient.nationality
-        })
-        const patientCreated = await newPatient.save();
-        res.status(201).json({
-            patient: patientCreated, 
-            personInfo: personCreated
-        });
-    } catch(e) {
-        res.status(409).json({message: e.message});
-    }
+    const newPatient = new Patient({
+        personId: personCreated._id,
+        clinicHistory: {
+            reason: clinicHistory.reason,
+            currentIllness: clinicHistory.currentIllness,
+            historyDesease: clinicHistory.historyDesease,
+            alcohol: clinicHistory.alcohol,
+            smoke: clinicHistory.smoke,
+            drugs: clinicHistory.drugs,
+            sexuality: clinicHistory.sexuality,
+            others: clinicHistory.others
+        },
+        code: patient.code,
+        allergies: patient.allergies,
+        address: patient.address,
+        birthday: patient.birthday,
+        occupation: patient.occupation,
+        civilStatus: patient.civilStatus,
+        nationality: patient.nationality
+    })
+    const patientCreated = await newPatient.save();
+
+    res.status(201).json({ patient: patientCreated, personInfo: personCreated });
 }
 
 const updatePatient = async (req, res) => {
@@ -77,35 +67,21 @@ const updatePatient = async (req, res) => {
     const { personId, clinicHistory, code, allergies, address, birthday, occupation, civilStatus, nationality } = patient;
     const { DNI, name, lastName, email, phone, sex } = patient.personInfo;
 
-    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No patient with id: ${id}`);
+    const updatedPerson = { DNI, name, lastName, email, phone, sex }; 
+    await Person.findOneAndUpdate({_id: personId}, updatedPerson, { new: true });
     
-    try {
-        const updatedPerson = { DNI, name, lastName, email, phone, sex }; 
-        if (!mongoose.Types.ObjectId.isValid(personId)) return res.status(404).send(`No person with id: ${personId}`);
-        await Person.findOneAndUpdate({_id: personId}, updatedPerson, { new: true });
-        
-        const updatedPatient = { code, allergies, address, birthday, occupation, civilStatus, nationality };
-        await Patient.findOneAndUpdate({_id: id}, updatedPatient, { new: true });
+    const updatedPatient = { code, allergies, address, birthday, occupation, civilStatus, nationality };
+    await Patient.findOneAndUpdate({_id: id}, updatedPatient, { new: true });
 
-        res.status(201).json("Patient updated successfully");
-    } catch(e) {
-        res.status(409).json({message: e.message});
-    }
+    res.status(201).json("Patient updated successfully");
 }
 
 const deletePatient = async (req, res) => {
     const { id } = req.params;
-
-    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No patient with id: ${id}`);
+    const updatedPatient = { active: false }; 
+    await Patient.findOneAndUpdate({_id: id}, updatedPatient, { new: true });
     
-    try {
-        const updatedPatient = { active: false }; 
-        await Patient.findOneAndUpdate({_id: id}, updatedPatient, { new: true });
-        
-        res.status(200).json("Patient deleted successfully");
-    } catch(e) {
-        res.status(409).json({message: e.message});
-    }
+    res.status(200).json("Patient deleted successfully");
 }
 
 module.exports = {
--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,33 +3,30 @@ const Person = require('../models/PersonModel')
 const Doctor = require('../models/DoctorModel')
 const User = require('../models/UserModel')
 const Specialty = require('../models/SpecialtyModel')
+const NotFoundError = require('../errors/NotFoundError')
 
 const getUsers = async (req, res) => {
-    try {
-        const users = await User.aggregate(
-            [
-                {
-                    $project: { username: 0, password: 0}
-                },
-                {
-                    $match: { active: { $eq: true }, role: { $ne: "DOCTOR"} }
-                },
-                {
-                    $lookup: {
-                        from: "people",
-                        localField: "personId",
-                        foreignField: "_id",
-                        as: "personInfo"
-                    }
-                },
-                {
-                    $unwind: "$personInfo"
+    const users = await User.aggregate(
+        [
+            {
+                $project: { username: 0, password: 0}
+            },
+            {
+                $match: { active: { $eq: true }, role: { $ne: "DOCTOR"} }
+            },
+            {
+                $lookup: {
+                    from: "people",
+                    localField: "personId",
+                    foreignField: "_id",
+                    as: "personInfo"
                 }
-            ])
-        res.status(200).json(users);
-    } catch(e) {
-        res.status(404).json({message: e.message});
-    }
+            },
+            {
+                $unwind: "$personInfo"
+            }
+        ])
+    res.status(200).json(users);
 }
 
 const createUser = async (req, res) => {
@@ -43,40 +40,37 @@ const createUser = async (req, res) => {
         phone: personInfo.phone, 
         sex: personInfo.sex
     })
-    
-    try {
-        const personCreated = await newPerson.save();
-        let specialtyReferenced = null;
 
-        let doctorCreated = null;
-        if (user.role === "DOCTOR") {
-            const newDoctor = new Doctor({
-                code: doctorInfo.code,
-                CMP: doctorInfo.CMP,
-                specialtyId: doctorInfo.specialtyId
-            })
-            specialtyReferenced = await Specialty.findOne({_id: doctorInfo.specialtyId});
-            if (!specialtyReferenced) return res.status(404).send(`No specialty with id: ${doctorInfo.specialtyId}`);
-            doctorCreated = await newDoctor.save();
-        }
+    const personCreated = await newPerson.save();
 
-        const newUser = new User({
-            personId: personCreated._id,
-            doctorId: doctorCreated ? doctorCreated._id : null,
-            username: user.username,
-            password: user.password,
-            role: user.role
+    let specialtyReferenced = null;
+    let doctorCreated = null;
+    if (user.role === "DOCTOR") {
+        const newDoctor = new Doctor({
+            code: doctorInfo.code,
+            CMP: doctorInfo.CMP,
+            specialtyId: doctorInfo.specialtyId
         })
-        const userCreated = await newUser.save();
-        res.status(201).json({
-            user: userCreated, 
-            doctorInfo: doctorCreated, 
-            personInfo: personCreated,
-            specialtyInfo: specialtyReferenced
-        });
-    } catch(e) {
-        res.status(409).json({message: e.message});
+        specialtyReferenced = await Specialty.findOne({_id: doctorInfo.specialtyId});
+        if (!specialtyReferenced) throw new NotFoundError(`No specialty with id: ${doctorInfo.specialtyId}`);
+        doctorCreated = await newDoctor.save();
     }
+
+    const newUser = new User({
+        personId: personCreated._id,
+        doctorId: doctorCreated ? doctorCreated._id : null,
+        username: user.username,
+        password: user.password,
+        role: user.role
+    })
+    const userCreated = await newUser.save();
+
+    res.status(201).json({
+        user: userCreated, 
+        doctorInfo: doctorCreated, 
+        personInfo: personCreated,
+        specialtyInfo: specialtyReferenced
+    });
 }
 
 const updateUser = async (req, res) => {
@@ -85,41 +79,26 @@ const updateUser = async (req, res) => {
     const { personId, doctorId, role } = user;
     const { DNI, name, lastName, email, phone, sex } = user.personInfo;
 
-    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No user with id: ${id}`);
-    
-    try {
-        const updatedPerson = { DNI, name, lastName, email, phone, sex }; 
-        if (!mongoose.Types.ObjectId.isValid(personId)) return res.status(404).send(`No person with id: ${personId}`);
-        await Person.findOneAndUpdate({_id: personId}, updatedPerson, { new: true });
+    const updatedPerson = { DNI, name, lastName, email, phone, sex }; 
+    await Person.findOneAndUpdate({_id: personId}, updatedPerson, { new: true });
 
-        if (role === "DOCTOR") {
-            const { code, CMP, specialtyId } = user.doctorInfo;
-            const updatedDoctor = { code, CMP, specialtyId };
-            if (!mongoose.Types.ObjectId.isValid(doctorId)) return res.status(404).send(`No doctor with id: ${doctorId}`);
-            const specialty = await Specialty.findOne({_id: specialtyId});
-            if (!specialty) return res.status(404).send(`No specialty with id: ${specialtyId}`);
+    if (role === "DOCTOR") {
+        const { code, CMP, specialtyId } = user.doctorInfo;
+        const updatedDoctor = { code, CMP, specialtyId };
+        const specialty = await Specialty.findOne({_id: specialtyId});
+        if (!specialty) throw new NotFoundError(`No specialty with id: ${specialtyId}`);
 
-            await Doctor.findOneAndUpdate({_id: doctorId}, updatedDoctor, { new: true });
-        }
-        res.status(201).json("User updated successfully");
-    } catch(e) {
-        res.status(409).json({message: e.message});
+        await Doctor.findOneAndUpdate({_id: doctorId}, updatedDoctor, { new: true });
     }
+    res.status(201).json("User updated successfully");
 }
 
 const deleteUser = async (req, res) => {
     const { id } = req.params;
-
-    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No user with id: ${id}`);
+    const updatedUser = { active: false }; 
+    await User.findOneAndUpdate({_id: id}, updatedUser, { new: true });
     
-    try {
-        const updatedUser = { active: false }; 
-        await User.findOneAndUpdate({_id: id}, updatedUser, { new: true });
-        
-        res.status(200).json("User deleted successfully");
-    } catch(e) {
-        res.status(409).json({message: e.message});
-    }
+    res.status(200).json("User deleted successfully");
 }
 
 module.exports = {
