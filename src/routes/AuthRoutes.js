5e07e9a82eaa470b02001cf95de5cdd2ce7a4e16
Diego Salas <diesalasmart@gmail.com>
2021-12-26 18:14:47 -0500

    create error files

---
+++ src/routes/AuthRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const { login } = require('../controllers/AuthController')
+
+const router = express.Router();
+
+router.post('/login', login);
+
+module.exports = router
