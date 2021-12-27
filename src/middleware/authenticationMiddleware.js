8f296cf1674a12cd6e31ee7ba48fa7f509fcdcd0
Diego Salas <diesalasmart@gmail.com>
2021-12-27 17:08:15 -0500

    rm console log

--- src/middleware/authenticationMiddleware.js
+++ src/middleware/authenticationMiddleware.js
@@ -10,7 +10,6 @@ const auth = async (req, res, next) => {
     const token = authHeader.split(' ')[1]
     try {
         const payload = jwt.verify(token, process.env.JWT_SECRET);
-        console.log(payload);
         next();
     } catch (error) {
         throw new UnauthenticatedError('Authentication invalid');
