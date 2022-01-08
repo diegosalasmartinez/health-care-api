9e987cc9aa1c675943842909c350b15c2aa4aef3
Diego Salas <diesalasmart@gmail.com>
2022-01-08 17:30:51 -0500

    search params in appointment get request

--- src/controllers/AppointmentController.js
+++ src/controllers/AppointmentController.js
@@ -2,10 +2,118 @@ const Appointment = require('../models/AppointmentModel');
 const { appointmentStatusObjects } = require('../utils');
 
 const getAppointments = async (req, res) => {
-    const appointments = await Appointment.find({
+    const { offset, limit, patient, doctor } = req.query;
+    const matchOptions = {
         active: { $eq: true }
-    })
-    res.status(200).json(appointments);
+    }
+    if (patient) {
+        matchOptions["$expr"] = {
+            "$regexMatch": {
+                "input": "$patientInfo.fullName",
+                "regex": '.*' + patient + '.*',
+                "options": "i"
+            }
+        }
+    }
+    if (doctor) {
+        matchOptions["$expr"] = {
+            "$regexMatch": {
+                "input": "$doctorInfo.fullName",
+                "regex": '.*' + doctor + '.*',
+                "options": "i"
+            }
+        }
+    }
+    const appointmentsResponse = await Appointment.aggregate([
+        {
+            $lookup: {
+                from: "patients",
+                localField: "patientId",
+                foreignField: "_id",
+                as: "patientInfo"
+            }
+        },
+        {
+            $unwind: "$patientInfo"
+        },
+        {
+            $lookup: {
+                from: "people",
+                localField: "patientInfo.personId",
+                foreignField: "_id",
+                as: "patientInfo.personInfo"
+            }
+        },
+        {
+            $unwind: "$patientInfo.personInfo"
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "doctorId",
+                foreignField: "_id",
+                as: "doctorInfo"
+            }
+        },
+        {
+            $unwind: "$doctorInfo"
+        },
+        {
+            $lookup: {
+                from: "people",
+                localField: "doctorInfo.personId",
+                foreignField: "_id",
+                as: "doctorInfo.personInfo"
+            }
+        },
+        {
+            $unwind: "$doctorInfo.personInfo"
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "secretaryId",
+                foreignField: "_id",
+                as: "secretaryInfo"
+            }
+        },
+        {
+            $unwind: "$secretaryInfo"
+        },
+        {
+            $lookup: {
+                from: "people",
+                localField: "secretaryInfo.personId",
+                foreignField: "_id",
+                as: "secretaryInfo.personInfo"
+            }
+        },
+        {
+            $unwind: "$secretaryInfo.personInfo"
+        },
+        {
+            $addFields: {
+                "patientInfo.fullName": { $concat: ["$patientInfo.personInfo.name", " ", "$patientInfo.personInfo.lastName"] },
+                "doctorInfo.fullName": { $concat: ["$doctorInfo.personInfo.name", " ", "$doctorInfo.personInfo.lastName"] }
+            }
+        },
+        {
+            $match: matchOptions
+        },
+        {
+            $facet: {
+                dataPrev: [ { $count: "count" } ],
+                data: [
+                    { $skip: parseInt(offset) },
+                    { $limit: parseInt(limit) }
+                ]
+            }
+        }
+    ])
+
+    const appointments = appointmentsResponse[0].data;
+    const numAppointments = appointmentsResponse[0].dataPrev.length > 0 ? appointmentsResponse[0].dataPrev[0].count : 0;
+    res.status(200).json({appointments, length: numAppointments});
 }
 
 const createAppointment = async (req, res) => {
